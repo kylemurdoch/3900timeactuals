0300414a5c05e1093fb82d22d72c0389e4471e6b
Kyle Murdoch <kylemurdoch@shaw.ca>
2019-09-22 15:13:36 -0700

    undo previous two commits

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
 
 //Handle production 
+if (process.env.NODE_ENV === 'production') {
     //Static folder
     app.use(express.static(__dirname + "/public"));
 
@@ -20,7 +21,8 @@ app.use('/api/posts', posts);
     app.get(/.*/, (req, res) => {
         res.sendFile(__dirname + "/public/index.html");
     });
+}
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
